e9ce1275d46de3632c66fc2f78aefe5425717769
FlaviusAetius <FlaviusAetius@users.noreply.github.com>
2016-03-09 20:32:20 +0000

    Add reordering example in C#

---
+++ 01-Threads-and-Locks/paul/reorder/reorder/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace reorder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new myCounter();
+
+            var thread1 = new Thread(() =>
+            {
+                while(true)
+                    counter.increment();
+            });
+            var thread2 = new Thread(() =>
+            {
+                while (true)
+                    if (counter.checkme())
+                        Console.WriteLine("Yikes! Re-ordering detected!");
+            });
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+        }
+    }
+
+    class myCounter
+    {
+        private int x, y;
+
+        public void increment()
+        {
+            x++;
+            y++;
+        }
+
+        public bool checkme()
+        {
+            return y > x;
+        }
+    }
+}
