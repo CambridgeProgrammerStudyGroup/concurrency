c2c97efb693a7bd46a3e8f789694f0c6026314fc
FlaviusAetius <FlaviusAetius@users.noreply.github.com>
2016-03-10 09:16:24 +0000

    Add C# example of lock objects for chapter 1

---
+++ 01-Threads-and-Locks/paul/ThreadingExample/ThreadingExample/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ThreadingExample
+{
+	public class Account
+	{
+		decimal balance;
+		public Account(decimal _balance)
+		{
+			balance = _balance;
+		}
+
+		readonly Object thisLock = new Object();
+
+		public void Withdraw(decimal amount)
+		{
+			lock (thisLock)
+			{
+				if (amount > balance)
+				{
+					throw new Exception("Insufficient funds");
+				}
+				balance -= amount;
+				Console.WriteLine("{0} withdrawn. new balance is {1}", amount, balance);
+			}
+		}
+	}
+}
+
