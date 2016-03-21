b748c0b1eacce5274c8bcc431e08f3a9c7a3cb86
FlaviusAetius <FlaviusAetius@users.noreply.github.com>
2016-03-21 13:13:54 +0000

    Add dining philosophers example with deadlock

---
+++ 01-Threads-and-Locks/paul/DiningPhilosophersDeadlock/DiningPhilosophersDeadlock/Chopstick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DiningPhilosophersDeadlock
+{
+    public class Chopstick
+    {
+        private int _id;
+
+        public Chopstick(int id)
+        {
+            _id = id;
+        }
+
+        public void Use(int philosopherId)
+        {
+            // just to show that something is going on
+            Console.WriteLine("Philosopher {0} is using chopstick {1}", philosopherId, _id);
+        }
+    }
+}
