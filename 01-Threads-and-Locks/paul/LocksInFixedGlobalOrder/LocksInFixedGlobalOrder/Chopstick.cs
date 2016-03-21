6eb834d262287916b267c5a0c8d69b4e49d12540
FlaviusAetius <FlaviusAetius@users.noreply.github.com>
2016-03-21 13:20:50 +0000

    Fix the dining philosophers by aquiring locks in a fixed, global order

---
+++ 01-Threads-and-Locks/paul/LocksInFixedGlobalOrder/LocksInFixedGlobalOrder/Chopstick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LocksInFixedGlobalOrder
+{
+    public class Chopstick
+    {
+        public int Id { get; }
+
+        public Chopstick(int id)
+        {
+            Id = id;
+        }
+
+        public void Use(int philosopherId)
+        {
+            // just to show that something is going on
+            Console.WriteLine("Philosopher {0} is using chopstick {1}", philosopherId, Id);
+        }
+    }
+}
---
+++ 01-Threads-and-Locks/paul/LocksInFixedGlobalOrder/LocksInFixedGlobalOrder/Philosopher.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace LocksInFixedGlobalOrder
+{
+    public class Philosopher
+    {
+        private readonly int _id;
+        private readonly Chopstick _first;
+        private readonly Chopstick _second;
+        private readonly Random _random;
+
+        public Philosopher(int id, Chopstick left, Chopstick right)
+        {
+            _id = id;
+            _first = left.Id < right.Id ? left : right;
+            _second = left.Id < right.Id ? right : left;
+            _random = new Random();
+        }
+
+        public void Dine()
+        {
+            while (true)
+            {
+                // thinking
+                Thread.Sleep(_random.Next(100));
+
+                lock (_first)
+                {
+                    lock (_second)
+                    {
+                        // eating
+                        _first.Use(_id);
+                        _second.Use(_id);
+                        Thread.Sleep(_random.Next(100));
+                    }
+                }
+            }
+        }
+    }
+}
